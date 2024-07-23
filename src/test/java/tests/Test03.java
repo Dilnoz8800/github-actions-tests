afb0a39acffa79312b6eb82e3dc39ecd95ddd5d7
dilnozaruzieva <dilnoz880055@gmail.com>
2024-07-23 13:57:45 -0700

    Created Test03

---
+++ src/test/java/tests/Test03.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Test03 {
+
+    @Test
+    public void test01(){
+        Assertions.assertEquals(5,5);
+    }
+}
